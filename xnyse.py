d02cd607e108908808e597f6d4ed9003c7e1b671
bgorm <bgorman@live.com>
2022-05-26 10:36:56 -0300

    rm cached

--- xnyse.py
+++
@@ -1,8 +0,0 @@
-with open("Stonks Files\\Input\\cse_file.txt", "r") as file:
-    lines = file.readlines()
-
-symbols = [line.replace("\n", "").split(".")[0] for line in lines if len(line.split(".")) <= 1]
-
-with open("Stonks Files\\Input\\cse_file.txt", "w") as file:
-    for line in symbols:
-        file.write(line + ".cn\n")
